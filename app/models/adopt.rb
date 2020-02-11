53c4c80e4c9bf7a0a7220c6a965e57d392fabed3
edhernandez04 <mchh.004@gmail.com>
2020-02-11 17:47:49 -0500

    adopt dependancy + unlist dog

--- app/models/adopt.rb
+++ app/models/adopt.rb
@@ -1,6 +1,6 @@
 class Adopt < ApplicationRecord
-    belongs_to :user
-    belongs_to :pet
+    belongs_to :user, dependent: :destroy
+    belongs_to :pet, dependent: :destroy
 
     validates :pet_id, uniqueness: true
 end
