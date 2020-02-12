65e4d57b5815db0d444b0fc1684f7e47e5cd2e37
edhernandez04 <mchh.004@gmail.com>
2020-02-12 09:43:03 -0500

    routes are screwed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,12 +15,9 @@ class UsersController < ApplicationController
   end
 
   def create
-
     @user = User.new(user_params)
-    
-    if @user.save &&
-      session[:user_id] = user.id
-      flash[:messages] = "New User Added"
+    if @user.save
+      session[:user_id] = @user.id
       redirect_to user_path(@user)
     else 
       flash[:messages] = @user.errors.full_messages[0]
--- config/routes.rb
+++ config/routes.rb
@@ -2,30 +2,20 @@ Rails.application.routes.draw do
 
   root 'home#home'
 
-  get '/users', to: 'users#index', as:'users'
-  get '/signup', to: 'users#new', as:'new_user'
-  post '/users', to: 'users#create'
-  get '/profile', to: 'users#show', as: 'user'
+  get '/users/login', to: 'session#new_user'
+  post '/users/sessions', to: 'session#create_user'
+  delete '/users/logout', to: 'session#destroy_user'
 
   get '/breeders/login', to: 'session#breeder_new'
   post '/breeders/sessions', to: 'session#breeder_create'
   delete '/breeders/logout', to: 'session#breeder_destroy'
 
-  get '/users/login', to: 'session#new_user'
-  post '/users/sessions', to: 'session#create_user'
-  delete '/users/logout', to: 'session#destroy_user'
-
   get '/pets/:id/adopted_new', to: 'pets#adopted_new', as: 'adopted_new'
   post '/pets/:id/adopted', to: 'pets#adopted', as: 'adopted'
 
-resources :centers
+resources :users
 resources :pets
 resources :breeders
-
-
-
-
-
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
